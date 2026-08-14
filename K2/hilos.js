9d7e7bda2d1f0c510ce913e4bbf87d4b0568bdbd
K2 <torresjonpersonal@gmail.com>
2026-08-14 12:02:50 -0400

    Add files via upload

---
+++ K2/hilos.js
@@ -0,0 +1,73 @@
+export const tutorialsData = [
+    {
+      id: 1, //<p>, <strong>, <em>, <code>, <pre>
+      title: "Microsoft Office 2016",
+      category: "Windows",
+      subcategory: "Software",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFPWBVIaKLBMrd3BDoHwv8u6t24LXapfZUtegf5bRj8g&s=10",
+      desc: "Descarga el paquete de Microsoft Office 2016 + Licencias",
+      content: `
+        <p><strong>Microsoft Office 2016</strong> es una suite de programas de oficina creada por Microsoft. Incluye aplicaciones clásicas como <strong>Word, Excel, PowerPoint y Outlook</strong>. Permite crear textos, hojas de cálculo, presentaciones y gestionar correos electrónicos para tareas escolares o de trabajo.</p>
+        <p><br /></p>
+        <p>El archivo está comprimido con la extensión <em>.zip</em> con el instalador y dos activadores dentro.</p>
+        <h3>Paso 1: Descomprensión e Instalación</h3>
+        <p>Para descomprimir el archivo <em>.zip</em> has <strong>"click derecho"</strong> sobre el archivo descargado y seleccionar la opción <strong>"extraer aquí"</strong>, luego busca dentro de la carpeta <strong>"Instalador"</strong> el archivo <strong>setup.exe</strong></p>
+        <h3>Paso 2: Post-Instalación y Activación de la licencia</h3>
+        <p>Luego de instalar Office 2016 dirigete a la segunda carpeta de extrajo la descarga, <strong>"Activación"</strong></p> y dentro habran dos activadores, ambos son automáticos, si alguno no funciona prueba con el siguiente.
+        <p><br /></p>
+        <img src="https://3clics.com.ve/images/stories/virtuemart/product/21.png" alt="Microsoft Office 2016">
+        <h3>|| DESCARGA(S)</h3>
+        <p><a href="https://www.mediafire.com/file/oc7qdu931txewwl/Off1c3_PRO_2016.zip/file" class="btn-sitio">Descargar</a></p>
+      `
+    },
+    {
+      id: 2,
+      title: "Control My Monitor",
+      category: "Windows",
+      subcategory: "Software",
+      image: "https://www.nirsoft.net/utils/multimonitortool.png",
+      desc: "Configuración avanzada sobre tu monitor incluso para solucionar problemas.",
+      content: `
+      
+        <p><strong>Control My Monitor</strong> s una herramienta gratuita para Windows desarrollada por <strong>NirSoft</strong> que permite ver y modificar los ajustes de un monitor (como brillo, contraste, nitidez y balance de colores) directamente desde el sistema operativo mediante software, usando el estándar DDC/CI y códigos VCP, sin necesidad de usar los botones físicos de la pantalla.</p>
+        <p><br /></p>
+        <img src="https://www.nirsoft.net/articles/turn-off-monitor.png" alt="Microsoft Office 2016">
+        <h3>|| DESCARGA(S)</h3>
+        <p><a href="https://www.mediafire.com/file/np9hb2dxolxejkn/controlmymonitor.zip/file" class="btn-sitio">Descargar</a></p>
+      `
+    },
+    {
+      id: 3,
+      title: "Bandicam (PORTABLE)",
+      category: "Windows",
+      subcategory: "Software",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ79bnzpq-BQaKPM-8Di3rkcbqM38ZAlNqsr_Re0dFaDg&s=10",
+      desc: "Captura juegos en 2D y 3D usando DirectX, OpenGL o Vulkan con mínimo retraso.",
+      content: `
+        
+        <p><strong>Bandicam</strong> es un programa de grabación de pantalla ligero y de alto rendimiento para Windows y Mac. Permite capturar la pantalla de la computadora, juegos y dispositivos externos como cámaras web.</p>
+        <p><br /></p>
+        <p>Es muy conocido por su uso de aceleración por hardware (NVENC, Quick Sync, AMD), lo que reduce el uso de CPU y RAM para grabar en alta resolución (hasta 4K Ultra HD) sin ralentizar el equipo.</p>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Bandicam-screen-recorder.png?utm_source=es.wikipedia.org&utm_campaign=index&utm_content=original" alt="WinRAR">
+        <h3>|| DESCARGA(S)</h3>
+        <p><a href="https://www.mediafire.com/file/egwzxunmhxnw1c9/B4ND1C4M.zip/file" class="btn-sitio">Descargar</a></p>
+      `
+    },
+    {
+      id: 4,
+      title: "WinRAR",
+      category: "Windows",
+      subcategory: "Software",
+      image: "https://www.win-rar.com/uploads/pics/rar-archive_89.png",
+      desc: "Potente herramienta de compresión y descompresión de archivos.",
+      content: `
+        
+        <p><strong>WinRAR</strong> es una potente herramienta de compresión y descompresión de archivos para Microsoft Windows, desarrollada por Eugene Roshal y win.rar GmbH (RARLAB). Permite crear y gestionar archivos en formatos RAR y ZIP, además de extraer contenido de formatos populares como 7z, CAB, ARJ, TAR, GZ e ISO. Es compatible con Windows 10 y 11, e incluye soporte para cifrado AES de 256 bits y algoritmos de verificación como BLAKE2.</p>
+        <p><br /></p>
+        <p>Es un programa "try before you buy" (prueba antes de comprar) con un periodo de evaluación de 40 días. <strong>Tras este lapso, las funciones básicas de descompresión siguen estando disponibles.</strong></p>
+        <img src="https://www.software-shop.com/images/productos/winrar/WinRAR_Screenshot_01.png" alt="WinRAR">
+        <h3>|| DESCARGA(S)</h3>
+        <p><a href="https://www.win-rar.com/download.html?&L=6" class="btn-sitio">Descargar (Sitio Oficial)</a></p>
+      `
+    }
+  ];
